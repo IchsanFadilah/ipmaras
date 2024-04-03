7c6b5a6c1577f0703e8af9a0fedfa4a09ab0c32c
ichsan fadilah <ichsanfadilah1@gmail.com>
2024-04-03 15:42:34 +0700

    update 03042024

--- main.js
+++ main.js
@@ -16,21 +16,3 @@
     }
   }
 })();
-
-// let image = document.getElementById("image");
-// let images = [
-//   "assets/img/1.png",
-//   "assets/img/2.png",
-//   "assets/img/3.png",
-//   "assets/img/4.png",
-//   "assets/img/5.png",
-// ];
-
-// setInterval(() => {
-//   console.log("image.src");
-// }, 2000);
-
-// // setInterval(function () {
-// //   let random = Math.floor(Math.random() * 5);
-// //   image.src = images[random];
-// // }, 1200);
