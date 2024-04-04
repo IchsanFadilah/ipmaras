b9e1325d49c8c465e9b0b1184178d8c2c4bd0980
ichsan fadilah <ichsanfadilah1@gmail.com>
2024-04-04 20:25:35 +0700

    exploring UI with Js

--- main.js
+++ main.js
@@ -6,6 +6,7 @@
     "assets/img/4.png",
     "assets/img/5.png",
   ];
+
   let ganti = 0;
   setInterval(imageChg, 1500);
   function imageChg() {
@@ -16,3 +17,43 @@
     }
   }
 })();
+
+// (function () {
+//   let bgColor = ["#f1601e", "dbc9ff", "#f7dbe3", "#91a8b3", "#01b679"];
+
+//   let ganti1 = 0;
+//   setInterval(bgChg, 1500);
+//   function bgChg() {
+//     document.querySelector("body").style.backgroundColor = bgColor[ganti1];
+//     setTimeout();
+//   }
+// });
+
+let bg = 0;
+let il = 0;
+
+function bgChg() {
+  let bgColors = document.getElementById("banner");
+  let imgIl = document.getElementById("image");
+  let bgColor = ["#f1601e", "#dbc9ff", "#f7dbe3", "#91a8b3", "#01b679"];
+  let images = [
+    "assets/img/1.png",
+    "assets/img/2.png",
+    "assets/img/3.png",
+    "assets/img/4.png",
+    "assets/img/5.png",
+  ];
+  bgColors.style.backgroundColor = bgColor[bg];
+  imgIl.setAttribute("src", images[il]);
+  // bg = (bg + 1) % bgColor.length;
+  // il = (il + 1) % images.length;
+  bg++;
+  if (bg === bgColor.length) {
+    bg = 0;
+  }
+  il++;
+  if (il === images.length) {
+    il = 0;
+  }
+}
+setInterval(bgChg, 1500);
